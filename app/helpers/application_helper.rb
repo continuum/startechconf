1774861c7ce61ae2d969278879d638cc26caae2d
shesho <chechonouvel@gmail.com>
2013-03-28 14:50:09 -0300

    further improvements, re-adding sponsor images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
 
   def locale_li(locale)
     content_tag('li', class: ('selected' if current_locale == locale)) do
-      #link_to t({'es' => :spanish, 'en' => :english, 'pt' => :portuguese}[locale]),
-              #url_for(locale: locale)
+      link_to t({'es' => :spanish, 'en' => :english, 'pt' => :portuguese}[locale]),
+              url_for(locale: locale)
     end
   end
 
