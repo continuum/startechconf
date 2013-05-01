4009d83d8db59e54bb9197a104a8f6be969e7db5
Coto <rodrigo.augosto@gmail.com>
2013-04-30 22:09:16 -0400

    bigger twitter button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
   end
 
   def preregistered_tweet_button
-    %q|<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.startechconf.com" data-text="Me acabo de pre-registrar por un 30% de descuento en la @StarTechConf." data-lang="es" data-count="none">Twittear</a>
+    %q|<a href="https://twitter.com/share" class="twitter-share-button" data-via="startechconf" data-url="http://www.startechconf.com" data-text="Me acabo de pre-registrar por un 30% de descuento en la @StarTechConf." data-lang="es" data-size="large" data-count="none">Comparte en Twitter</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>|.html_safe
   end
 
