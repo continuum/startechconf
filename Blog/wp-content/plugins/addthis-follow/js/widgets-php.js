13dfdb4b01b90b76ad25b917c9dc5bd636de43fb
Coto <rodrigo.augosto@gmail.com>
2013-08-08 17:34:18 -0400

    remove blog

--- Blog/wp-content/plugins/addthis-follow/js/widgets-php.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $('.atmorelink').live('click', function(e){
-        e.preventDefault();
-        $('.atfollowservice').filter('.atmore').toggleClass('hidden');
-        $(this).find('.atmore').toggleClass('hidden');
-        $(this).find('.atless').toggleClass('hidden');
-    });
-})(jQuery);
