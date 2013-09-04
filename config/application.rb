a9f3d8ac07b2b25d9599ca3cae39cec42fd2b683
shesho <chechonouvel@gmail.com>
2013-09-04 11:38:02 -0400

    single yml for speakers

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module Startechconf
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.fallbacks = true
     config.i18n.default_locale = 'es-CL'
 
     # Configure the default encoding used in templates for Ruby 1.9.
